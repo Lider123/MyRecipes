12e6b01ae03334f8f400898da35c6b699c7ee5b0
babaetskv <babaetskv@intersvyaz.net>
2019-08-22 10:55:36 +0500

    Replace model constructors with javascript

--- src/models/Ingredient.js
+++ src/models/Ingredient.js
@@ -1,31 +1,5 @@
-export default class Ingredient {
-  constructor(name, count) {
-    this._id = "" + Math.floor(1e8 * Math.random());
-    this._name = name;
-    this._count = count;
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  set id(value) {
-    this._id = value;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
-  }
-
-  get count() {
-    return this._count;
-  }
-
-  set count(value) {
-    this._count = value;
-  }
+export default function Ingredient(name, count) {
+  this.id = "" + Math.floor(1e8 * Math.random());
+  this.name = name;
+  this.count = count;
 }
