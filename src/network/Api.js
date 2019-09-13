ade1ffa0c90fe5edfa10aa0c50a677d293d5cb57
babaetskv <babaetskv@intersvyaz.net>
2019-09-13 15:26:45 +0500

    Set up privacy rules

--- src/network/Api.js
+++ src/network/Api.js
@@ -1,28 +1,41 @@
 import * as Serializers from "./Serializers";
+import firebase from "react-native-firebase";
 
 export default class Api {
   static BASE_API = "https://firestore.googleapis.com/v1/projects/myrecipes-39edd/databases/(default)/documents";
 
   static getRecipes() {
-    return fetch(Api.BASE_API + "/recipes")
+    return firebase.auth().currentUser.getIdToken(true)
+      .then((idToken) => fetch(Api.BASE_API + "/recipes", {
+        method: "GET",
+        headers: {
+          'Authorization': `Bearer ${idToken}`,
+        },
+      }))
       .then((response) => response.json());
   }
 
   static createOrUpdateRecipe(recipe) {
     const data = Serializers.serializeRecipe(recipe);
-    return fetch(Api.BASE_API + "/recipes/" + recipe.id, {
-      method: 'PATCH',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+    return firebase.auth().currentUser.getIdToken(true)
+      .then((idToken) => fetch(Api.BASE_API + "/recipes/" + recipe.id, {
+        method: 'PATCH',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${idToken}`
+        }
+      }));
   }
 
   static deleteRecipe(id) {
-    return fetch(Api.BASE_API + "/recipes/" + id, {
-      method: "DELETE",
-    })
+    return firebase.auth().currentUser.getIdToken(true)
+      .then((idToken) => fetch(Api.BASE_API + "/recipes/" + id, {
+        method: "DELETE",
+        headers: {
+          'Authorization': `Bearer ${idToken}`,
+        },
+      }))
       .then((response) => response.json());
   }
 }
